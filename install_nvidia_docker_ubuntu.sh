802e9cb0d78328a4d3386bb11ddf8374ba5b3634
Peter Bryzgalov <peterbryz@yahoo.com>
2017-03-03 18:42:39 +0900

    Fixed tag bug.

--- install_nvidia_docker_ubuntu.sh
+++ install_nvidia_docker_ubuntu.sh
@@ -2,6 +2,7 @@
 # Install Nvidia-docker on Ubuntu EC2 instance (tested on P2).
 
 sudo apt-get update
+sudo apt install -y nvidia-modprobe
 
 # Install Cuda
 sudo apt-get install -y nvidia-cuda-toolkit
@@ -17,7 +18,4 @@ docker --version
 # Install nvidia-docker and nvidia-docker-plugin
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
-
-sudo apt install -y nvidia-modprobe
-
 
